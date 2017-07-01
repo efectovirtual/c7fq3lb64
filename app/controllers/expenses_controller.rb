54e3ab105173e141d00750f6625e69762e958cf5
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-07-01 15:24:24 -0500

    Ajustes en el filtro de búsqueda

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,6 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+     # @expenses = Expense.order("date DESC")
+     @expenses = Expense.where("concept LIKE ? AND category_id LIKE ?", "%#{params[:concept]}%", "%#{params[:category_id]}")
   end
 end
